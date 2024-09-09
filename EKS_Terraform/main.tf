621455586420b570e42ffe3d3389195e560eef54
ARUNMAHANWAR <81462975+ARUNMAHANWAR@users.noreply.github.com>
2024-09-09 15:21:03 +0530

    Update main.tf

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -6,14 +6,14 @@ resource "aws_vpc" "devopsshack_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "devopsshack-vpc"
+    Name = "AWS-VPC"
   }
 }
 
 resource "aws_subnet" "devopsshack_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.devopsshack_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.AWS-VPC.id
+  cidr_block              = cidrsubnet(aws_vpc.AWS-VPC.cidr_block, 8, count.index)
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
